5142a04043396961557f31fb6af95e8de2d41050
Volkan GENÇ <gencvolkan@gmail.com>
2019-02-03 08:38:18 -0800

    Add project files.

---
+++ Yield/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+
+namespace Yield
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (int i in Power(2, 8))
+            {
+                Console.WriteLine("a={0}  ", i.ToString());
+            }
+
+            Console.ReadLine();
+
+        }
+
+
+        public static IEnumerable Power(int number, int exponent)
+        {
+            int counter = 0;
+            int result = 1;
+
+            while (counter++ < exponent)
+            {
+                result = result * number;
+                yield return result;
+            }
+        }
+
+    }
+
+}
